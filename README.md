27be64e98a6c5e7e88a07e4d044c46504e10b779
Aigul <aigulagish@gmail.com>
2020-12-27 23:36:26 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # text_generation
+
+This pet project allows to generate data in Shakespeare's style. Neural network is a key part of this project. The network uses LSTM to generate texts.
---
+++ model.py
@@ -0,0 +1,29 @@
+class Model(nn.Module):
+    def __init__(self, n_vocab, lstm_size, embedding_dim, num_layers):
+        super(Model, self).__init__()
+        self.lstm_size = lstm_size
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+
+        self.embedding = nn.Embedding(
+            num_embeddings=n_vocab,
+            embedding_dim=self.embedding_dim,
+        )
+        self.lstm = nn.LSTM(
+            input_size=self.lstm_size,
+            hidden_size=self.lstm_size,
+            num_layers=self.num_layers,
+            dropout=0.2,
+            batch_first=True
+        )
+        self.fc = nn.Linear(self.lstm_size, n_vocab)
+
+    def forward(self, x, prev_state):
+        embed = self.embedding(x)
+        output, state = self.lstm(embed, prev_state)
+        logits = self.fc(output)
+        return logits, state
+
+    def init_state(self, sequence_length):
+        return (torch.zeros(self.num_layers, sequence_length, self.lstm_size),
+                torch.zeros(self.num_layers, sequence_length, self.lstm_size))
